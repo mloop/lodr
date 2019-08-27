8b02ba48a4c172fdc07873d5c7c23412be25621e
Kevin Donovan <43554362+kmdono02@users.noreply.github.com>
2019-08-27 09:52:01 -0400

    Delete arms01a.m

--- arms_method/arms01a.m
+++
@@ -1,7 +0,0 @@
-arms01a:  arms_main01a.o arms.o
-	gcc -o arms01a arms_main01a.o arms.o -lm -lc
-arms_main01a.o: arms.h arms_main01a.c
-	gcc -c arms_main01a.c -o arms_main01a.o
-arms.o: arms.c
-	gcc -c arms.c -o arms.o 
-
